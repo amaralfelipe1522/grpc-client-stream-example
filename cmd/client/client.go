418cfadf0426a75168d47ad3156434c54410a49d
Felipe Amaral <amaral.felipe@live.com>
2022-05-04 17:52:23 -0300

    Conclusão do gRPC Client Stream

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	"github.com/amaralfelipe1522/grpc-client-stream-example/pb"
 	"google.golang.org/grpc"
@@ -19,7 +20,8 @@ func main() {
 
 	client := pb.NewUserServiceClient(connection)
 	//AddUser(client)
-	AddUserVerbose(client)
+	//AddUserVerbose(client)
+	AddUsers(client)
 }
 
 // Request
@@ -46,4 +48,45 @@ func AddUserVerbose(client pb.UserServiceClient) {
 		}
 		fmt.Println("Status: ", stream.Status)
 	}
+}
+
+func AddUsers(client pb.UserServiceClient) {
+	reqs := []*pb.User{
+		&pb.User{
+			Id:    "0",
+			Name:  "Felipe Amaral de Souza",
+			Email: "amaral.felipe@protonmail.com",
+		},
+		&pb.User{
+			Id:    "1",
+			Name:  "Gustavo Henrique Freitas",
+			Email: "gustavo.souza@protonmail.com",
+		},
+		&pb.User{
+			Id:    "2",
+			Name:  "Leandro Machado Siqueira",
+			Email: "leandro.siqueira@protonmail.com",
+		},
+		&pb.User{
+			Id:    "3",
+			Name:  "Kaique Spagnol Tofoli",
+			Email: "kaique.tofoli@protonmail.com",
+		},
+	}
+
+	stream, err := client.AddUsers(context.Background()) // Garante que se a mensagem não for chegar, a requisição será encerrada
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	for _, req := range reqs {
+		stream.Send(req)
+		time.Sleep(time.Second * 3)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error receiiving response: %v", err)
+	}
+	fmt.Println(res)
 }
